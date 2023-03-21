7a73f8e736052ffb974f9d234f53281abca40fa0
Yao Weijie <yaowj.it@outlook.com>
2023-03-21 21:27:15 +0800

    cmp-rime user data folder

---
+++ .local/share/cmp-rime/install.sh
@@ -0,0 +1,4 @@
+# debian 系
+sudo apt install librime-bin rime-data-double-pinyin -y
+sudo apt remove librime-plugin-lua -y
+cp ~/.local/share/cmp-rime/user.yaml.bak ~/.local/share/cmp-rime/user.yaml

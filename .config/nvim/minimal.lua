4ce30448c23df584cda521d410c2a44447b51501
Yao Weijie <yaowj.it@outlook.com>
2023-10-16 00:24:59 +0800

    daily update

--- .config/nvim/minimal.lua
+++ .config/nvim/minimal.lua
@@ -5,22 +5,5 @@ require("helper.lazy").setup({
     -- plugins and configs
     spec = {
         { "folke/lazy.nvim", version = "*" },
-        {
-            "nvim-treesitter/nvim-treesitter",
-            dependencies = {
-                "HiPhish/nvim-ts-rainbow2",
-            },
-            config = function()
-                require("nvim-treesitter.configs").setup({
-                    ensure_installed = { "c", "cpp", "vim", "lua", "luadoc" },
-                    highlight = {
-                        enable = true,
-                    },
-                    rainbow = {
-                        enable = true,
-                    },
-                })
-            end,
-        },
     },
 })

f2b2508aae08e28b6cdb6fa74fde6e3841d4fec6
Yao Weijie <yaowj.it@outlook.com>
2023-11-15 18:01:44 +0800

    apt: sed cmd

---
+++ .config/apt/sed.sh
@@ -0,0 +1,8 @@
+# 清华源
+sed -i "s/archive.ubuntu.com/mirrors.tuna.tsinghua.edu.cn/g" /etc/apt/sources.list
+
+# 中科大
+sed -i "s/archive.ubuntu.com/mirrors.ustc.edu.cn/g" /etc/apt/sources.list
+
+# 阿里云
+sed -i "s/archive.ubuntu.com/mirrors.aliyun.com/g" /etc/apt/sources.list
